250e373d2233f066f92f86da4f7584b80dbd1a28
vpasias <52623837+vpasias@users.noreply.github.com>
2022-01-15 20:04:26 +0200

    Update vm_deployment.sh

--- isp6/vm_deployment.sh
+++ isp6/vm_deployment.sh
@@ -74,6 +74,7 @@ ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo apt update && sudo apt upg
 ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo DEBIAN_FRONTEND=noninteractive apt-get install linux-generic-hwe-20.04 --install-recommends -y"
 ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo apt autoremove -y && sudo apt --fix-broken install -y"
 
+ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "curl -O https://releases.hashicorp.com/vagrant/2.2.8/vagrant_2.2.8_x86_64.deb && sudo apt install -y ./vagrant_2.2.8_x86_64.deb --allow-downgrades"
 ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo apt-get update && sudo apt-get install -y python3-pip"
 ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "sudo python3 -m pip install netsim-tools"
 ssh -o "StrictHostKeyChecking=no" ubuntu@netsim "netlab install ubuntu ansible libvirt -y -q"
